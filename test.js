0fd3b5b2d142406cac464a4b196e5deb1719d17b
hzz780 <hzz780@qq.com>
2018-12-05 15:14:23 +0800

    add rpc API get_txs_result

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var Aelf = require('./lib/aelf.js');
-var aelf = new Aelf(new Aelf.providers.HttpProvider("http://localhost:1234/chain"));
+// var aelf = new Aelf(new Aelf.providers.HttpProvider("http://localhost:1234/chain"));
+// var aelf = new Aelf(new Aelf.providers.HttpProvider("http://192.168.197.23:8000/chain"));
+var aelf = new Aelf(new Aelf.providers.HttpProvider("http://172.31.5.155:8000/chain"));
 
 // var tokenc = aelf.chain.getContractAbi('0x75b19ac4415c072512d011634ac86a9c58cf');
 aelf.chain.connectChain(function (err, result) {
@@ -16,11 +18,57 @@ var wallet = Aelf.wallet.getWalletByPrivateKey('30e65ca0db9bba3242086af94fd15123
 var tokenc = aelf.chain.contractAt('30748a6c59c1309eacd1eaf86feb3ea16074', wallet);
 
 
+// 获取区块信息1
+// 44282 44289
+aelf.chain.getBlockInfo(44282, true, async (err, result) => {
+    // console.log(result.result.Body.Transactions);
+    console.log(result);
+    let transactions = result.result.Body.Transactions;
+
+    let transactionPromises = [];
+    let txLength = transactions.length;
+    // Get transactionPromises
+    for (let i = 0; i < txLength; i++) {
+        transactionPromises.push(new Promise((resolve, reject) => {
+            aelf.chain.getTxResult(transactions[i], (error, result) => {
+
+                if (error || !result) {
+                    console.log('error result getTxResult: ', listIndex, result, error);
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        }));
+    }
+
+    Promise.all(transactionPromises).then(async (result) => {
+        console.log('result:', result);
+    });
+});
+
+// 获取区块信息2
+aelf.chain.getBlockInfo(44289, true, async (err, result) => {
+    // console.log(result.result.Body.Transactions);
+    console.log(result);
+});
+
+// 获取区块信息3
+var transations = aelf.chain.getTxsResultByBlockhash(
+    '0d0d7dd9dd6fafbc176d900a1d78dc7a1aa2ecebfe72e6d22d2d9c3e84a5ee9d',
+    0,
+    100
+);
+
+// 获取交易
+var txResult = aelf.chain.getTxResult('a9695243e41f845a72700cb06b3afeb0241d80f80e4627833a5c7b6feeded6c2');
+// var txResult = aelf.chain.getTxResult('538e05cd43c91a505c60002b07d976c0c4e95738f284d7ab60546ee88781622b');
+
 // console.log('TokenName: ', tokenc.TokenName());
 //
 // console.log('BalanceOf: ', tokenc.BalanceOf('0x0424421b90fdacf9470ee049f50113a56656'));
 
-console.log('Tranfer: ', tokenc.Transfer('70b721ac293f0d3787561828688d860dde4a', 10));
+// console.log('Tranfer: ', tokenc.Transfer('70b721ac293f0d3787561828688d860dde4a', 10));
 
 // let contractAbi = aelf.chain.getContractAbi('0xdb458e5db5db1b0ecad3408acc344c96794c');
 // console.log('contractAbi: ', contractAbi);
