11e3630270c5f2f19afc042841ba571692c36aff
swk <shiwk@tju.edu.cn>
2018-12-12 14:24:02 +0800

    add more functionalities

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
-
-module.exports = {
-	mode: 'production',
-	// mode: 'development',
-	entry: './lib/aelf.js',
-	output: {
-		path: path.resolve(__dirname, 'output'), // equal to __diname + '/build'
-        filename: 'aelf.web.js'
-	}
-}
+// const path = require('path');
+//
+// module.exports = {
+// 	mode: 'production',
+// 	// mode: 'development',
+// 	entry: './lib/aelf.js',
+// 	output: {
+// 		path: path.resolve(__dirname, 'output'), // equal to __diname + '/build'
+//         filename: 'aelf.web.js'
+// 	}
+// }
