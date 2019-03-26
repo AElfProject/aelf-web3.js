709d7de9ca524effc5afe449781c2979621c20fd
hzz780 <hzz780@qq.com>
2019-03-26 11:00:29 +0800

    return unpack output when callReadOnly in method1

--- devDemos/2.0/demo01.js
+++ devDemos/2.0/demo01.js
@@ -2,7 +2,7 @@
  * @file demo01.js
  * @author huangzongzhe
  */
-
+/* eslint-disable fecs-camelcase */
 const Aelf = require('../../lib/aelf.js');
 const Wallet = require('../../lib/aelf/wallet');
 
@@ -16,31 +16,48 @@ const privateKey = '097fc2e1eea2bfe258e7962c644f6d87ac58bfbd80301e10740baf52f451
 
 const wallet = Wallet.getWalletByPrivateKey(defaultPrivateKey);
 
-const aelf = new Aelf(new Aelf.providers.HttpProvider('http://192.168.197.56:8000/chain'));
+// const aelf = new Aelf(new Aelf.providers.HttpProvider('http://192.168.197.56:8000/chain'));http://192.168.197.56:8101/chain
+// const aelf = new Aelf(new Aelf.providers.HttpProvider('http://192.168.197.56:8101/chain'));
+const aelf = new Aelf(new Aelf.providers.HttpProvider('http://192.168.197.70:8000/chain'));
 
-const tokenC = aelf.chain.contractAt('3AhZRe8RvTiZUBdcqCsv37K46bMU2L2hH81JF8jKAnAUup9', wallet);
-const resourceC = aelf.chain.contractAt('2Xg2HKh8vusnFMQsHCXW1q3vys5JxG5ZnjiGwNDLrrpb9Mb', wallet);
+const tokenC = aelf.chain.contractAt('4rkKQpsRFt1nU6weAHuJ6CfQDqo6dxruU3K3wNUFr6ZwZYc', wallet);
 
-tokenC.Symbol();
-tokenC.BalanceOf('65dDNxzcd35jESiidFXN5JV8Z7pCwaFnepuYQToNefSgqk9');
-aelf.chain.getBlockInfo('4137', true);
-// 1089, 4137
-// 6823187769516bd9f7a1e15f58cbba8c9613262b6cc196cd3488ecdb5ae1177c
-var txsResults;
-aelf.chain.getTxsResult(
-    '63af6a24bc6e41915b3b3042dabc3b2e2c47cb6fcab7f50f0971bc1bdaa9f7cf',
-    0,
-    100,
-    function (error, result) {
-        console.log('tx result: ', error, result);
-        txsResults = result;
-    }
-);
+tokenC.GetBalance({symbol:'AELF', owner: '65dDNxzcd35jESiidFXN5JV8Z7pCwaFnepuYQToNefSgqk9'});
+console.log(11111);
+tokenC.GetBalance.call({
+    symbol: 'ELF',
+    owner: '58h3RwTfaE8RDpRNMAMiMv8jUjanCeYHBzKuQfHbrfSFTCn'
+}, (err, result) => {
+    
+    console.log(err, result);
+})
+/* eslint-enable */
+tokenC.Transfer({
+    symbol: 'ELF',
+    to: '58h3RwTfaE8RDpRNMAMiMv8jUjanCeYHBzKuQfHbrfSFTCn',
+    amount: '1000'
+});
+// const resourceC = aelf.chain.contractAt('2Xg2HKh8vusnFMQsHCXW1q3vys5JxG5ZnjiGwNDLrrpb9Mb', wallet);
 
-aelf.chain.getTxResult('70d3db416a0fc3b96f383f9dcc7bc27909eeef3d54bd5869479e6c9486bb0912').Transaction;
+// tokenC.Symbol();
+// tokenC.BalanceOf('65dDNxzcd35jESiidFXN5JV8Z7pCwaFnepuYQToNefSgqk9');
+// aelf.chain.getBlockInfo('4137', true);
+// // 1089, 4137
+// // 6823187769516bd9f7a1e15f58cbba8c9613262b6cc196cd3488ecdb5ae1177c
+// var txsResults;
+// aelf.chain.getTxsResult(
+//     '63af6a24bc6e41915b3b3042dabc3b2e2c47cb6fcab7f50f0971bc1bdaa9f7cf',
+//     0,
+//     100,
+//     function (error, result) {
+//         console.log('tx result: ', error, result);
+//         txsResults = result;
+//     }
+// );
 
+// aelf.chain.getTxResult('70d3db416a0fc3b96f383f9dcc7bc27909eeef3d54bd5869479e6c9486bb0912').Transaction;
 
-// Resource
-const resourceC = aelf.chain.contractAt('2Xg2HKh8vusnFMQsHCXW1q3vys5JxG5ZnjiGwNDLrrpb9Mb', wallet);
-resourceC.GetElfBalance('RAM');
-resourceC.GetUserBalance('286izE4MsFWdGZn2xCcPSBaRqwXFB95xu5Urfki3q5xmmCc', 'CPU');
+// // Resource
+// const resourceC = aelf.chain.contractAt('2Xg2HKh8vusnFMQsHCXW1q3vys5JxG5ZnjiGwNDLrrpb9Mb', wallet);
+// resourceC.GetElfBalance('RAM');
+// resourceC.GetUserBalance('286izE4MsFWdGZn2xCcPSBaRqwXFB95xu5Urfki3q5xmmCc', 'CPU');
