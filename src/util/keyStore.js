75a8a7528b6caa7177fcbfb4470eb170272ff39f
atom-young <atom.yang@aelf.io>
2020-12-02 16:12:47 +0800

    fix: 🐛 use browserify-aes MODES config

--- src/util/keyStore.js
+++ src/util/keyStore.js
@@ -8,6 +8,225 @@ import randomBytes from 'randombytes';
 import { keccak256 } from './hash';
 import { KEY_STORE_ERRORS } from '../common/constants';
 
+const AES_MODES = {
+  'aes-128-ecb': {
+    cipher: 'AES',
+    key: 128,
+    iv: 0,
+    mode: 'ECB',
+    type: 'block',
+
+  },
+  'aes-192-ecb': {
+    cipher: 'AES',
+    key: 192,
+    iv: 0,
+    mode: 'ECB',
+    type: 'block',
+
+  },
+  'aes-256-ecb': {
+    cipher: 'AES',
+    key: 256,
+    iv: 0,
+    mode: 'ECB',
+    type: 'block',
+
+  },
+  'aes-128-cbc': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  'aes-192-cbc': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  'aes-256-cbc': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  aes128: {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  aes192: {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  aes256: {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CBC',
+    type: 'block',
+
+  },
+  'aes-128-cfb': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CFB',
+    type: 'stream',
+
+  },
+  'aes-192-cfb': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CFB',
+    type: 'stream',
+
+  },
+  'aes-256-cfb': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CFB',
+    type: 'stream',
+
+  },
+  'aes-128-cfb8': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CFB8',
+    type: 'stream',
+
+  },
+  'aes-192-cfb8': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CFB8',
+    type: 'stream',
+
+  },
+  'aes-256-cfb8': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CFB8',
+    type: 'stream',
+
+  },
+  'aes-128-cfb1': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CFB1',
+    type: 'stream',
+
+  },
+  'aes-192-cfb1': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CFB1',
+    type: 'stream',
+
+  },
+  'aes-256-cfb1': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CFB1',
+    type: 'stream',
+
+  },
+  'aes-128-ofb': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'OFB',
+    type: 'stream',
+
+  },
+  'aes-192-ofb': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'OFB',
+    type: 'stream',
+
+  },
+  'aes-256-ofb': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'OFB',
+    type: 'stream',
+
+  },
+  'aes-128-ctr': {
+    cipher: 'AES',
+    key: 128,
+    iv: 16,
+    mode: 'CTR',
+    type: 'stream',
+
+  },
+  'aes-192-ctr': {
+    cipher: 'AES',
+    key: 192,
+    iv: 16,
+    mode: 'CTR',
+    type: 'stream',
+
+  },
+  'aes-256-ctr': {
+    cipher: 'AES',
+    key: 256,
+    iv: 16,
+    mode: 'CTR',
+    type: 'stream',
+
+  },
+  'aes-128-gcm': {
+    cipher: 'AES',
+    key: 128,
+    iv: 12,
+    mode: 'GCM',
+    type: 'auth',
+
+  },
+  'aes-192-gcm': {
+    cipher: 'AES',
+    key: 192,
+    iv: 12,
+    mode: 'GCM',
+    type: 'auth',
+
+  },
+  'aes-256-gcm': {
+    cipher: 'AES',
+    key: 256,
+    iv: 12,
+    mode: 'GCM',
+    type: 'auth',
+
+  }
+};
+
 const defaultOptions = {
   dklen: 32,
   n: 8192, // 2048 4096 8192 16384
@@ -42,7 +261,8 @@ export function getKeystore(
   const { cipher = 'aes-128-ctr' } = opt;
   const sliceLength = /128/.test(cipher) ? 16 : 32;
   const salt = randomBytes(32); // instance of Buffer
-  const iv = randomBytes(16); // instance of Buffer
+  const ivLength = (AES_MODES[cipher.toLowerCase()] || {}).iv;
+  const iv = randomBytes(ivLength === undefined ? 16 : ivLength); // instance of Buffer
   const derivedKey = scrypt(
     Buffer.from(password, 'utf8'),
     salt,
