c2077c3dfb9297d7d834d79ef046654bf1320d27
AbigailDeng <Abigail.deng@ienyan.com>
2023-04-14 13:23:18 +0800

    feat: coverageReporters

--- jest.node.config.js
+++ jest.node.config.js
@@ -46,7 +46,7 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['json-summary'],
+  coverageReporters: ['text', 'json-summary'],
   // coverageReporters: [
   //   "json",
   //   "text",
